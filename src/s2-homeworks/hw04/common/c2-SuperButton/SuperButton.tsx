4cfdbabe5861bc9965929fa3ba78933468b57ddb
KasyanovaMari <magivara0@gmail.com>
2025-05-18 23:24:35 +0600

    sprint2week4done

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,25 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    let finalClassName = s.button
+    if (!disabled) {
+        if (xType === 'red') {
+            finalClassName += ` ${s.red}`;
+        } else if (xType === 'secondary') {
+            finalClassName += ` ${s.secondary}`;
+        } else {
+            finalClassName += ` ${s.default}`;
+        }
+    }
+    if (disabled) {
+        finalClassName += ` ${s.disabled}`;
+    }
+    if (className) {
+        finalClassName += ` ${className}`;
+    }
+
+
+    // задачка на смешивание классов
 
     //ИЛИ ЕСЛИ НЕ ПОНЯТНО С finalClassName  ТОЖЕ САМОЕ ПРИ ПОМОЩИ ШАБЛОННЫХ СТРОК:
     // `${s.СТИЛЬ КНОПКИ}  ${xType==='КРАСНЫЙ' ? ДАВАЙ КРАСНЫЙ СТИЛЬ : xType === 'secondary' ? ДАВАЙ СЕКОНДАРИ СТИЛЬ: ДАВАЙ ПО ДЕФОЛТУ } ${disabled ? ДАВАЙ ДИЗАБЛЕТ СТИЛЬ :  ПУСТУЮ СТРОКУ} `
